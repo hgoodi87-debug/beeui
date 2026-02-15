9821873ea27e6c25028d6c2af60e965c55142ada
dbcjsaud-ycm <dbcjsaud@gmail.com>
2026-02-15 18:59:55 +0900

    feat: update hero background video to final elite bee version (v39.4)

--- client/components/LandingRenewal.tsx
+++ client/components/LandingRenewal.tsx
@@ -35,7 +35,7 @@ const CONFIG = {
         dark: "#0C0C0C",
     },
     video: {
-        src: "https://firebasestorage.googleapis.com/v0/b/beeliber-main.firebasestorage.app/o/vc%2F0215%20(1).mp4?alt=media&token=d673a42d-a906-48e1-bd8d-a36d93781eac",
+        src: "https://firebasestorage.googleapis.com/v0/b/beeliber-main.firebasestorage.app/o/vc%2Fbee.mp4?alt=media&token=1d445016-6586-4922-bd40-ff7c677ec125",
     },
     sizes: [
         { label: "M", range: "18-23\"", height: "h-20" },
